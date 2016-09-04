36b8b651bccc4a0c4e2e2a334f9fa25427552380
Malcolm Kyle McCullum <mmccullum1991@gmail.com>
2016-09-04 18:18:54 -0500

    add Google login feature

--- ContactManager/Startup.cs
+++ ContactManager/Startup.cs
@@ -9,6 +9,10 @@ namespace ContactManager
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
+            app.UseGoogleAuthentication(
+                clientId: "663441201333-2r4h1d98aqie8v1c5752sla5b8ipal0l.apps.googleusercontent.com",
+                clientSecret: "UFsmuIPEHJDfKjdBdoJJ5TJ5");
         }
     }
 }
