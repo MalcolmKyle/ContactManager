07552327a3292da3b1d7e9e1f10e0f383155d027
Malcolm Kyle McCullum <mmccullum1991@gmail.com>
2016-09-04 18:48:41 -0500

    add Facebook log-in feature

--- ContactManager/Startup.cs
+++ ContactManager/Startup.cs
@@ -13,6 +13,10 @@ namespace ContactManager
             app.UseGoogleAuthentication(
                 clientId: "663441201333-2r4h1d98aqie8v1c5752sla5b8ipal0l.apps.googleusercontent.com",
                 clientSecret: "UFsmuIPEHJDfKjdBdoJJ5TJ5");
+
+            app.UseFacebookAuthentication(
+                appId: "1229528817093789",
+                appSecret: "40b4d45bf263186a24ded681f1e4e420");
         }
     }
 }
