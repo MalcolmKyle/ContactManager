da19b970a51e569ca4a799108286ece99971d6f6
Malcolm Kyle McCullum <mmccullum1991@gmail.com>
2016-09-04 21:25:56 -0500

    add canEdit role

--- ContactManager/Migrations/Configuration.cs
+++ ContactManager/Migrations/Configuration.cs
@@ -5,6 +5,8 @@ namespace ContactManager.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
     using ContactManager.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
 
     internal sealed class Configuration : DbMigrationsConfiguration<ContactManager.Models.ApplicationDbContext>
     {
@@ -13,8 +15,33 @@ namespace ContactManager.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
+        bool AddUserAndRole(ContactManager.Models.ApplicationDbContext context)
+        {
+            IdentityResult ir;
+            var rm = new RoleManager<IdentityRole>
+                (new RoleStore<IdentityRole>(context));
+            // Create new role manager with editing permissions
+            // More information on ASP.NET roles: http://www.asp.net/identity/overview/features-api
+      
+            ir = rm.Create(new IdentityRole("canEdit"));
+
+            var um = new UserManager<ApplicationUser>(
+            new UserStore<ApplicationUser>(context));
+                var user = new ApplicationUser()
+                {
+                    UserName = "malcolm@contactmanager.com",
+                };
+                ir = um.Create(user, "Password_1234");
+                if (ir.Succeeded == false)
+                    return ir.Succeeded;
+                ir = um.AddToRole(user.Id, "canEdit");
+                return ir.Succeeded;
+        }
+
         protected override void Seed(ContactManager.Models.ApplicationDbContext context)
         {
+            AddUserAndRole(context);
+
             context.Contacts.AddOrUpdate(p => p.Name,
                 new Contact
                 {
